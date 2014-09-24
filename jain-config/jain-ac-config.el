9cbcc22adfa5cce4edb75c4ce7aeac21310202f6
Jain.Y <jain_y@126.com>
2014-09-24 10:37:03 +0800

    Update jain-ac-config.el

--- jain-config/jain-ac-config.el
+++ jain-config/jain-ac-config.el
@@ -1,19 +1,25 @@
-
-(add-to-list 'load-path "~/my-lisps/auto-complete-1.3/")
+(add-to-list 'load-path "~/my-lisps/auto-complete-1.3.1/")
+(require 'auto-complete)  
+(add-to-list 'ac-dictionary-directories "~/my-lisps/auto-complete-1.3.1/dict")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/my-lisps/auto-complete-1.3/dict")
 (ac-config-default)
 
 (setq ac-auto-start nil)
 ;; (setq ac-show-menu-timer 0.5)
 (setq ac-quick-help-delay 0.5)
 
+(require 'auto-complete-clang-async)
+;; (require 'auto-complete-clang)
+
 (defun my-ac-config ()
-  ;; (setq ac-clang-flags (split-string "-I/mingw/include 
-  ;;                                              -I/mingw/lib/gcc/mingw32/4.5.0/include/c++ 
-  ;;                                              -I/mingw/lib/gcc/mingw32/4.5.0/include/c++/backward                                                
-  ;;                                              -I/mingw/lib/gcc/mingw32/4.5.0/include/c++/mingw32
-  ;;                                              -I/mingw/lib/gcc/mingw32/4.5.0/include-fixed"))
+  (setq ac-clang-flags (list "-I/mingw/include" 
+                                      "-I/mingw/lib/gcc/mingw32/4.8.1/include/c++"
+                                      "-I/mingw/lib/gcc/mingw32/4.8.1/include/c++/backward"
+                                      "-I/mingw/lib/gcc/mingw32/4.8.1/include/c++/migw32"
+                                      "-I/mingw/lib/gcc/mingw32/4.8.1/include-fixed"
+                                      "-ID:/Program Files/Microsoft Visual Studio/VC98/Include"
+                                      "-ID:/Program Files/Microsoft Visual Studio/VC98/ATL/Include"
+                                      "-ID:/Program Files/Microsoft Visual Studio/VC98/MFC/Include"))
   (setq-default ac-sources '(ac-source-abbrev
                              ac-source-dictionary
                              ac-source-words-in-same-mode-buffers))
@@ -24,14 +30,20 @@
   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
   (global-auto-complete-mode t))
 
-(defun my-ac-cc-mode-setup ()  
-  (setq ac-sources (append ac-sources
-;;  (setq ac-sources
-                           '(ac-source-semantic
-                             ac-source-semantic-raw
-                             ac-source-yasnippet))))
 
-(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+(defun my-ac-clang-async-mode-setup ()
+   (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+   (setq ac-sources '(ac-source-clang-async))
+   (ac-clang-launch-completion-process)
+)
+
+(defun my-ac-clang-mode-setup ()
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources))
+)
+
+;;(add-hook 'c-mode-common-hook 'my-ac-clang-async-mode-setup)
+(add-hook 'c-mode-common-hook 'my-ac-clang-mode-setup)
+
 ;; ac-source-gtags  
 (my-ac-config)
 
@@ -47,19 +59,3 @@
 ;; (ac-set-trigger-key ".")
 
 (provide 'jain-ac-config)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
